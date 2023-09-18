fbfb7a35b102b8a480dc3aa87f8c41914cb54ef7
sahari <mjsahari@gmail.com>
2023-09-17 23:16:25 -0700

    cleanup

--- cmd/common.go
+++
@@ -1,7 +0,0 @@
-package cmd
-
-//func check(err error) {
-//	if err != nil {
-//		log.Fatalf("Something went wrong: %q", err)
-//	}
-//}
--- cmd/refresh.go
+++ cmd/refresh.go
@@ -13,13 +13,6 @@ import (
 	"github.com/spf13/cobra"
 )
 
-//const (
-//	// LastUsagePath is the pathing to the cached last usage
-//	LastUsagePath string = "/.aws/sso/cache/last-usage.json"
-//	// WindowsLastUsagePath is the pathing to cached last usage in Windows
-//	WindowsLastUsagePath string = "\\.aws\\sso\\cache\\last-usage.json"
-//)
-
 var (
 	refreshCmd = &cobra.Command{
 		Use:   "refresh",
@@ -38,22 +31,6 @@ var (
 func init() {
 	rootCmd.AddCommand(refreshCmd)
 }
-
-// LastUsageInformation contains the info on last usage
-//type LastUsageInformation struct {
-//	AccountID   string `json:"account_id"`
-//	AccountName string `json:"account_name"`
-//	Role        string `json:"role"`
-//}
-//
-//// LastUsageFile returns the path to the last usage file
-//func LastUsageFile() string {
-//	homeDir, _ := os.UserHomeDir()
-//	if runtime.GOOS == "windows" {
-//		return fmt.Sprintf("%s%s", homeDir, WindowsLastUsagePath)
-//	}
-//	return fmt.Sprintf("%s%s", homeDir, LastUsagePath)
-//}
 
 // RefreshCredentials is used to refresh credentials
 func RefreshCredentials(oidcClient *ssooidc.Client, ssoClient *sso.Client) {
@@ -104,27 +81,3 @@ func RefreshCredentials(oidcClient *ssooidc.Client, ssoClient *sso.Client) {
 	log.Printf("Assumed role: %s", *roleName)
 	log.Printf("Credentials expire at: %s\n", time.Unix(roleCredentials.RoleCredentials.Expiration/1000, 0))
 }
-
-// SaveUsageInformation is used to write usage information to file
-//func SaveUsageInformation(accountInfo *types.AccountInfo, roleInfo *types.RoleInfo) {
-//	homeDir, _ := os.UserHomeDir()
-//	target := homeDir + "/.aws/sso/cache/last-usage.json"
-//	usageInformation := LastUsageInformation{
-//		AccountID:   *accountInfo.AccountId,
-//		AccountName: *accountInfo.AccountName,
-//		Role:        *roleInfo.RoleName,
-//	}
-//	WriteStructToFile(usageInformation, target)
-//}
-//
-//func readUsageInformation() (*LastUsageInformation, error) {
-//	homeDir, _ := os.UserHomeDir()
-//	bytes, err := os.ReadFile(homeDir + "/.aws/sso/cache/last-usage.json")
-//	if err != nil {
-//		return nil, err
-//	}
-//	lui := new(LastUsageInformation)
-//	err = json.Unmarshal(bytes, lui)
-//	check(err)
-//	return lui, nil
-//}
