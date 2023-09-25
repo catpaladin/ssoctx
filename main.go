fb5b6f2fb048e74bc755512d2972fb5c77d2ae9e
Mike <mjsahari@gmail.com>
2023-09-24 21:42:52 -0700

    add build info commit to command

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "aws-sso-util/cmd"
+import (
+	"aws-sso-util/cmd"
+	"fmt"
+	"runtime/debug"
+)
 
 func main() {
+	var commit string
+	buildInfo, _ := debug.ReadBuildInfo()
+	for _, setting := range buildInfo.Settings {
+		if setting.Key == "vcs.revision" {
+			commit = setting.Value
+		}
+	}
+	fmt.Printf("Commit: %s\n", commit)
 	cmd.Execute()
 }
